8e6359b6dab360e81634a6f9c866b548a161636c
XanderVar <xander_var@mail.ru>
2021-12-06 23:38:12 +0300

    created: added keboard control for swiper slide

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@ const swiper = new Swiper('.swiper', {
     nextEl: ".slider-button--next",
     prevEl: ".slider-button--prev",
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  keypress (Swiper, keyCode) {},
 });
