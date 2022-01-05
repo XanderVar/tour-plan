29262e070fd9f19a7cb43e898aa0e57672c96ef9
XanderVar <xander_var@mail.ru>
2022-01-06 00:12:41 +0300

    create: animated tour-plan

--- js/main.js
+++ js/main.js
@@ -113,8 +113,8 @@ $(".form").each(function () {
 
 $('.input-phone').mask('+7 (999) 999-99-99', {placeholder: "Phone Number*"}, {'translation': {9: {pattern: /[0-9]/}}});
 
-
-
+ AOS.init();
+ 
 });
 
 
